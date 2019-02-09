7c31ede08c46dcf537d684099c8b9e3b58f9938b
BOG-A209-E-025 <BOG-A209-E-025@RGH-LS6-25.UMD.LOCAL.EDU>
2019-02-09 08:03:18 -0500

    Impresion tiempo

---
+++ claseMaven/src/main/java/com/mycompany/clasemaven/Inicio.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.clasemaven;
+
+import org.joda.time.LocalTime;
+
+
+
+/**
+ *
+ * @author BOG-A209-E-025
+ */
+public class Inicio {
+    
+    public static void main(String[] args) {
+        LocalTime tiempo = new LocalTime();
+        System.out.println("El tiempo es: "+tiempo);
+    }
+    
+}
